58b3171499ee043823c887e8455a4b4201a3b378
Damon <4496289+drbree82@users.noreply.github.com>
2023-04-29 09:34:07 +1200

    Update jobbies.py

--- jobbies.py
+++ jobbies.py
@@ -1,3 +1,112 @@
+import requests
+import json
+import os
+from bs4 import BeautifulSoup
+from feedgen.feed import FeedGenerator
+from flask import Flask, make_response
+
+app = Flask(__name__)
+
+urls = [
+    'https://www.flexjobs.com/jobs',
+    'https://remote.co/remote-jobs',
+    'https://workingnomads.co/jobs',
+    'https://weworkremotely.com',
+    'https://jobspresso.co/browsejobs',
+    'https://www.virtualvocations.com/jobs',
+    'https://www.freelancer.com/jobs',
+    'https://www.skipthedrive.com/jobs',
+    'https://www.peopleperhour.com/freelance-jobs',
+    'https://www.guru.com/d/jobs'
+]
+
+def scrape_flexjobs(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    job_listings = []
+    job_elems = soup.select('div.job.row.result')
+    for job_elem in job_elems:
+        job_title_elem = job_elem.select_one('div.col-md-5 > h5 > a')
+        job_title = job_title_elem.get_text(strip=True)
+        job_url = f"https://www.flexjobs.com{job_title_elem['href']}"
+
+        company_elem = job_elem.select_one('div.col-md-3 > span > a')
+        company = company_elem.get_text(strip=True)
+
+        date_elem = job_elem.select_one('div.col-md-2 > span')
+        date = date_elem.get_text(strip=True)
+
+        job_listings.append({
+            'title': job_title,
+            'company': company,
+            'date': date,
+            'url': job_url
+        })
+
+    return job_listings
+
+def scrape_remote_co(url):
+    # Implement the scraping code for Remote.co
+    pass
+
+def scrape_working_nomads(url):
+    # Implement the scraping code for Working Nomads
+    pass
+
+def scrape_weworkremotely(url):
+    # Implement the scraping code for We Work Remotely
+    pass
+
+def scrape_jobspresso(url):
+    # Implement the scraping code for Jobspresso
+    pass
+
+def scrape_virtualvocations(url):
+    # Implement the scraping code for Virtual Vocations
+    pass
+
+def scrape_freelancer(url):
+    # Implement the scraping code for Freelancer.com
+    pass
+
+def scrape_skipthedrive(url):
+    # Implement the scraping code for SkipTheDrive
+    pass
+
+def scrape_peopleperhour(url):
+    # Implement the scraping code for PeoplePerHour
+    pass
+
+def scrape_guru(url):
+    # Implement the scraping code for Guru.com
+    pass
+
+def scrape_jobs():
+    all_job_listings = []
+
+    # Uncomment and replace each function with the correct scraping code for each website
+    # Make sure to also import the necessary libraries for each function
+
+    # Example: Scrape FlexJobs
+    job_listings = scrape_flexjobs(urls[0])
+    all_job_listings.extend(job_listings)
+
+    return all_job_listings
+
+def read_previous_jobs(file_path):
+    if os.path.exists(file_path):
+        with open(file_path, 'r') as file:
+            return json.load(file)
+    else:
+        return []
+
+def write_new_jobs(file_path, new_jobs):
+    with open(file_path, 'w') as file:
+        json.dump(new_jobs, file)
+
+def generate_rss
+
 def generate_rss_feed(job_listings):
     fg = FeedGenerator()
     fg.title('Latest Remote Jobs')
